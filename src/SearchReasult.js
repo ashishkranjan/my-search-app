0ad1bb0086a83a5dc9d7aa0ca6bb14996bb0ba8a
Ashish Kumar Ranjan <ranjan.ashish1988@gmail.com>
2021-02-10 21:13:11 +0530

    Delete old file

--- src/SearchReasult.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SearchResult = (props) => {
-    return <ul key={props.geoname.geonameId}>
-             <li key={props.geoname.countryId}>{props.index + 1}. {props.geoname.name} - {props.geoname.countryName}</li>
-           </ul>;
-};
-
-export default SearchResult;
